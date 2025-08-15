5c1948ae7e720874c91478416d4324c110ba177d
Amaan <moamaanuddin@gmail.com>
2025-08-15 14:09:03 +0530

    Ready for github

--- todo_main/settings.py
+++ todo_main/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
+from decouple import config
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,12 +21,16 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-pdag0h6pwfyk@-8ytw@pw2eaj^t_0g^!lymf5i=(i^yrc=%qfz'
+#To generate a secret key
+#python -c "from django.core.management.utils import get_random_secret_key; print(get_random_secret_key())"
+#or add a random secure key in .env file
+
+SECRET_KEY=config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG',cast=bool,default=False)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 
 
 # Application definition
